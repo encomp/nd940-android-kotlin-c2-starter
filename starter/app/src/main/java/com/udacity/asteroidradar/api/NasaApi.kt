a638fa064cc701eba020a4b1ab8b985f45c0b3a1
encomp <edgar.martinez.rico@gmail.com>
2021-10-10 20:35:22 -0700

    Add

--- starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApi.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/api/NasaApi.kt
@@ -1,4 +1,52 @@
 package com.udacity.asteroidradar.api
 
-class NasaApi {
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.PictureOfDay
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://api.nasa.gov/"
+private val API_KEY = "[API_KEY_HERE]"
+
+/**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+/**
+ * Use the Retrofit builder to build a retrofit object using a Moshi converter with our Moshi
+ * object.
+ */
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+/**
+ * A public interface that exposes the [getAsteroids] and [getImageOfTheDay] method
+ */
+interface NasaApiService {
+    @GET("neo/rest/v1/feed")
+    fun getAsteroids(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("api_key") apiKey: String = API_KEY
+    ): Call<String>
+
+    @GET("planetary/apod")
+    fun getImageOfTheDay(@Query("api_key") apiKey: String = API_KEY): Call<PictureOfDay>
+}
+
+/**
+ * A public Api object that exposes the lazy-initialized Retrofit service
+ */
+object NasaApi {
+    val retrofitService : NasaApiService by lazy { retrofit.create(NasaApiService::class.java) }
 }
