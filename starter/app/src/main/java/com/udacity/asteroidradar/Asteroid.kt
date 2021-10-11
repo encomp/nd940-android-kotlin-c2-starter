ca86bb842b6144ee849df7ef692c8d336264d326
encomp <edgar.martinez.rico@gmail.com>
2021-10-10 20:32:53 -0700

    Add room dependency.

--- starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -1,10 +1,35 @@
-package com.udacity.asteroidradar
+package com.udacity.asteroidradar.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.parcelize.Parcelize
 
+@Entity(tableName = "asteroid")
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+data class Asteroid(
+    @PrimaryKey
+    val id: Long = 0L,
+
+    @ColumnInfo(name = "codeName")
+    val codename: String,
+
+    @ColumnInfo(name = "closeApproachDate")
+    val closeApproachDate: String,
+
+    @ColumnInfo(name = "absoluteMagnitude")
+    val absoluteMagnitude: Double,
+
+    @ColumnInfo(name = "estimatedDiameter")
+    val estimatedDiameter: Double,
+
+    @ColumnInfo(name = "relativeVelocity")
+    val relativeVelocity: Double,
+
+    @ColumnInfo(name = "distanceFromEarth")
+    val distanceFromEarth: Double,
+
+    @ColumnInfo(name = "isPotentiallyHazardous")
+    val isPotentiallyHazardous: Boolean
+) : Parcelable
