4a16ce1a162da68df277e9d098d7c28d7d96851a
Edgar Rico <edgar.martinez.rico@gmail.com>
2022-03-11 13:06:57 -0800

    Fix MainViewModel.

--- starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.udacity.asteroidradar.main
 
 import androidx.lifecycle.*
 import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.Constants.API_QUERY_DATE_FORMAT
 import com.udacity.asteroidradar.PictureOfDay
 import com.udacity.asteroidradar.api.NasaRepository
@@ -48,14 +49,50 @@ class MainViewModel(private val asteroidDao: AsteroidDao) : ViewModel() {
                 // Add 7 Days as we want to get asteroids in a week span
                 currentDate.add(Calendar.DATE, 7)
                 val formattedEndDate = formatter.format(currentDate.time)
-                emit(
-                    Outcome.success(
-                        asteroidDao.getAsteroidsByWeek(
-                            formattedStartDate,
-                            formattedEndDate
-                        )
+                val outcome = Outcome.success(
+                    asteroidDao.getAsteroidsByWeek(
+                        formattedStartDate,
+                        formattedEndDate
                     )
                 )
+                emit(outcome)
+            } catch (ioException: Exception) {
+                emit(
+                    Outcome.error(
+                        "Unable to retrieve the asteroids: $ioException.message",
+                        ioException
+                    )
+                )
+            }
+        }
+    }
+
+    fun getAsteroidsFromDatabaseByDate() = liveData<Outcome<List<Asteroid>>> {
+        withContext(Dispatchers.IO) {
+            emit(Outcome.loading())
+            try {
+                val currentDateTime = Calendar.getInstance().time
+                val formatter = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.US)
+                val formattedDate = formatter.format(currentDateTime)
+                val outcome = Outcome.success(asteroidDao.getAsteroidsByDate(formattedDate))
+                emit(outcome)
+            } catch (ioException: Exception) {
+                emit(
+                    Outcome.error(
+                        "Unable to retrieve the asteroids: $ioException.message",
+                        ioException
+                    )
+                )
+            }
+        }
+    }
+
+    fun getAllAsteroidsFromDatabase() = liveData<Outcome<List<Asteroid>>> {
+        withContext(Dispatchers.IO) {
+            emit(Outcome.loading())
+            try {
+                val outcome = Outcome.success(asteroidDao.getAllAsteroids())
+                emit(outcome)
             } catch (ioException: Exception) {
                 emit(
                     Outcome.error(
